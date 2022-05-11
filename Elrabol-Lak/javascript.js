3cdf39b4aeb59fd34b64315bf2a3b7ed691bd1a0
Cziperlan <czipri.gergo.bertalan@students.jedlik.eu>
2022-05-11 11:01:48 +0200

    A galléria kész

--- Elrabol-Lak/javascript.js
+++ Elrabol-Lak/javascript.js
@@ -29,3 +29,41 @@ function panelek(db){
     document.getElementById('osszteljesitmeny').value = db * 275;
     document.getElementById('tetofelulet').value = db * 3.5
 }
+
+
+
+var elements = document.getElementsByClassName("column");
+
+var i;
+
+function one() {
+    for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "100%";  
+    elements[i].style.flex = "100%";
+  }
+}
+
+function two() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "50%";  
+    elements[i].style.flex = "50%";
+  }
+}
+
+function four() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "25%";  
+    elements[i].style.flex = "25%";
+  }
+}
+
+var header = document.getElementById("myHeader");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
